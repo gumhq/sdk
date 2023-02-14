d422178d883112f1b9bd55bab38ef776e34fceef
Vijay Gupta <vjgupta57@gmail.com>
2023-02-15 02:09:57 +0530

    refactor: reaction hook

--- packages/react-sdk/src/hooks/reaction/useReaction.ts
+++ packages/react-sdk/src/hooks/reaction/useReaction.ts
@@ -3,69 +3,78 @@ import { useState, useCallback } from "react";
 import { PublicKey } from "@solana/web3.js";
 import { ReactionType } from "@gumhq/sdk/lib/reaction";
 
-const useReaction = (sdk: SDK, fromProfile: PublicKey, toPostAccount: PublicKey, userAccount: PublicKey, owner: PublicKey) => {
+const useReaction = (sdk: SDK, ) => {
+  const [reactionPDA, setReactionPDA] = useState<PublicKey | null>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const handleSubmitTransaction = useCallback(async (connection: any) => {
-    setLoading(true);
-    setError(null);
-
-    try {
-      await connection.instructionMethodBuilder.rpc();
-    } catch (err: any) {
-      setError(err);
-    } finally {
-      setLoading(false);
-    }
-  }, []);
-
   const createReaction = useCallback(
-    async (reactionType: ReactionType) => {
+    async (reactionType: ReactionType, fromProfile: PublicKey, toPostAccount: PublicKey, userAccount: PublicKey, owner: PublicKey) => {
       setLoading(true);
+      setError(null);
+
+      try {
+        const ixMethodBuilder = await createReactionIxMethodBuilder(reactionType, fromProfile, toPostAccount, userAccount, owner);
+        await ixMethodBuilder?.rpc();
+        
+      } catch (err: any) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    }, [sdk]);
+
+  const createReactionIxMethodBuilder = useCallback(
+    async (reactionType: ReactionType, fromProfile: PublicKey, toPostAccount: PublicKey, userAccount: PublicKey, owner: PublicKey) => {
       setError(null);
 
       try {
         const data = await sdk.reaction.create(fromProfile, toPostAccount, reactionType, userAccount, owner);
-        return { 
-          instructionMethodBuilder: data?.instructionMethodBuilder,
-          submitTransaction: data.instructionMethodBuilder ? handleSubmitTransaction(data) : undefined,
-          reactionPDA: data.reactionPDA,
-          loading: false, 
-          error: null
-        };
+        setReactionPDA(data.reactionPDA);
+        return data.instructionMethodBuilder;
       } catch (err: any) {
         setError(err);
-      } finally {
-        setLoading(false);
+        return null;
       }
-    }, [sdk, fromProfile, toPostAccount, userAccount, owner]);
+    }, [sdk]);
+
 
   const deleteReaction = useCallback(
-    async (reactionAccount: PublicKey) => {
+    async (reactionAccount: PublicKey, fromProfile: PublicKey, toPostAccount: PublicKey, userAccount: PublicKey, owner: PublicKey) => {
       setLoading(true);
       setError(null);
 
       try {
-        const data = sdk.reaction.delete(reactionAccount, fromProfile, toPostAccount, userAccount, owner);
-        
-        return {
-          instructionMethodBuilder: data,
-          submitTransaction: data ? handleSubmitTransaction(data) : undefined,
-          loading: false,
-          error: null
-        };
-
+        const ixMethodBuilder = await deleteReactionIxMethodBuilder(reactionAccount, fromProfile, toPostAccount, userAccount, owner);
+        await ixMethodBuilder?.rpc();
       } catch (err: any) {
         setError(err);
       } finally {
         setLoading(false);
       }
-    }, [sdk, fromProfile, toPostAccount, userAccount, owner]);
+    }, [sdk]);
+
+    const deleteReactionIxMethodBuilder = useCallback(
+      async (reactionAccount: PublicKey, fromProfile: PublicKey, toPostAccount: PublicKey, userAccount: PublicKey, owner: PublicKey) => {
+        setError(null);
+
+        try {
+          const data = sdk.reaction.delete(reactionAccount, fromProfile, toPostAccount, userAccount, owner);
+          return data;
+        } catch (err: any) {
+          setError(err);
+          return null;
+        }
+      }, [sdk]);
+
+
 
   return { 
     createReaction,
+    createReactionIxMethodBuilder,
     deleteReaction,
+    deleteReactionIxMethodBuilder,
+    reactionPDA,
     loading,
     error
   };
