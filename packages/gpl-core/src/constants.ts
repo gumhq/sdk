b29fec5949c46af58c0e6336b9bfc9a0129d6add
Vijay Gupta <vjgupta57@gmail.com>
2023-02-22 16:54:36 +0530

    refactor(@gumhq/sdk): update program id for  devnet and localnet
    Updated the program ID of Gpl Core program to CDDMdCAWB5AXgvEy7XJRggAu37QPG1b9aJXndZoPUkkm in the  package

--- packages/gpl-core/src/constants.ts
+++ packages/gpl-core/src/constants.ts
@@ -5,10 +5,10 @@ export const GPLCORE_PROGRAMS = {
     "CDDMdCAWB5AXgvEy7XJRggAu37QPG1b9aJXndZoPUkkm"
   ),
   devnet: new anchor.web3.PublicKey(
-    "CE5LZTE7SZeaf65qid8zcvqJCM2nPJmhzMFW9C1y54WB"
+    "CDDMdCAWB5AXgvEy7XJRggAu37QPG1b9aJXndZoPUkkm"
   ),
   localnet: new anchor.web3.PublicKey(
-    "4KnnwKYMEsHid1NL3LKKS3z2Y2iXRVwfDSRdQPWwJ1Em"
+    "CDDMdCAWB5AXgvEy7XJRggAu37QPG1b9aJXndZoPUkkm"
   ),
   testnet: new anchor.web3.PublicKey(
     "9XZ9Y9Z9Y9Z9Y9Z9Y9Z9Y9Z9Y9Z9Y9Z9Y9Z9Y9Z9Y9Z"
