92c0c8ade1508b6445d7a0fb377def4067c8ab81
Vijay Gupta <vjgupta57@gmail.com>
2023-02-24 02:27:50 +0530

    feat(@gumhq/react-sdk): add Feed hooks
    Added useFeed and useExploreFeed hooks to react sdk. useFeed hook returns posts by the profiles you follow and useExploreFeed returns all the posts for a particular namespace.

---
+++ packages/react-sdk/src/hooks/post/useFeed.ts
@@ -0,0 +1,33 @@
+import { SDK } from "@gumhq/sdk";
+import { useState, useEffect, useCallback } from "react";
+import { PublicKey } from "@solana/web3.js";
+
+const useFeed = (sdk: SDK, profileAccount: PublicKey) => {
+  const [feedData, setFeedData] = useState<any>(null);
+  const [feedLoading, setFeedLoading] = useState(false);
+  const [feedError, setFeedError] = useState<Error | null>(null);
+
+  const fetchFeed = useCallback(
+    async () => {
+      setFeedLoading(true);
+      setFeedError(null);
+
+      try {
+        const data = await sdk.post.getPostsByFollowedUsers(profileAccount);
+
+        setFeedData(data);
+      } catch (err: any) {
+        setFeedError(err);
+      } finally {
+        setFeedLoading(false);
+      }
+    }, [sdk, profileAccount]);
+
+  useEffect(() => {
+    fetchFeed();
+  }, [fetchFeed]);
+
+  return { feedData, feedLoading, feedError };
+};
+
+export { useFeed };
