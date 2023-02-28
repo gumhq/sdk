cafaef3cd255b38819d805950f918a434afbc1be
Vijay Gupta <vjgupta57@gmail.com>
2023-02-28 15:40:07 +0530

    feat(@gumhq/react-sdk): export feed hooks

--- packages/react-sdk/src/hooks/index.ts
+++ packages/react-sdk/src/hooks/index.ts
@@ -1,6 +1,6 @@
 export { useUser, useCreateUser } from "./user";
 export { useProfile, useCreateProfile } from "./profile";
-export { usePost, useCreatePost } from "./post";
+export { usePost, useCreatePost, useFeed, useExploreFeed } from "./post";
 export { useReaction } from "./reaction";
 export { useFollow, useUnfollow } from "./connection";
 export { useGum } from "./useGum";
--- packages/react-sdk/src/hooks/post/index.ts
+++ packages/react-sdk/src/hooks/post/index.ts
@@ -1,2 +1,4 @@
 export { usePost } from "./usePost";
 export { useCreatePost } from "./useCreatePost";
+export { useFeed } from "./useFeed";
+export { useExploreFeed } from "./useExploreFeed";
