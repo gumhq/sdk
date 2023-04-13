811c87d8a1c44adeaed4b51a0e595570584507eb
Vijay Gupta <vjgupta57@gmail.com>
2023-04-13 15:25:47 +0530

    feat(storage): add support for second storage account owner and make storage immutable for shadow storage

--- packages/react-sdk/src/hooks/storage/shdw/useShadow.ts
+++ packages/react-sdk/src/hooks/storage/shdw/useShadow.ts
@@ -23,10 +23,17 @@ import { SessionWalletInterface } from 'src/hooks/session';
 export interface ShadowStorageInterface {
   uploadData: (data: File | ShadowFile, storageAccount?: PublicKey) => Promise<ShadowUploadResponse | null | undefined>;
   getOwnedStorageAccounts: () => Promise<StorageAccountResponse[]>;
-  createStorageAccount: (name: string, sizeInMB: number) => Promise<CreateStorageResponse | null>;
+  createStorageAccount: (name: string, sizeInMB: number, owner2?: PublicKey) => Promise<CreateStorageResponse | null>;
   shadowStorage: ShdwDrive | null;
 }
 
+/**
+ * Custom React hook to interact with the Shadow Drive storage system.
+ *
+ * @param wallet - The wallet object, which should be of type WalletContextState or SessionWalletInterface.
+ * @param connection - The connection object, which should be of type Connection from the @solana/web3.js package.
+ * @returns An object containing methods to upload data, manage storage accounts, and interact with the Shadow token (SHDW).
+ */
 export const useShadowStorage = (
   wallet: WalletContextState | SessionWalletInterface,
   connection: Connection,
@@ -34,6 +41,9 @@ export const useShadowStorage = (
   const [shadowStorage, setShadowStorage] = useState<ShdwDrive | null>(null);
   const SHDW = new PublicKey('SHDWyBxihqiCj6YekG2GUr7wqKLeLAMK1gHZck9pL6y');
 
+  /**
+   * Initialize the Shadow Drive instance when the wallet is connected.
+   */
   useEffect(() => {
     const initializeShadow = async () => {
       const newShadow = await new ShdwDrive(connection, wallet).init();
@@ -45,12 +55,25 @@ export const useShadowStorage = (
     }
   }, [connection.rpcEndpoint, wallet]);
 
+  /**
+   * Fetch the list of owned storage accounts.
+   *
+   * @returns A promise that resolves to an array of StorageAccountResponse objects.
+   */
   const getOwnedStorageAccounts = useCallback(async () => {
     return shadowStorage ? await shadowStorage.getStorageAccounts("v2") : [];
   }, [shadowStorage]);
 
-  const createStorageAccount = useCallback(async (name: string, sizeInMB: number) => {
-    return shadowStorage ? await shadowStorage.createStorageAccount(name, `${sizeInMB.toString()}MB`, "v2") : null;
+  /**
+   * Create a new storage account.
+   *
+   * @param name - The name of the new storage account.
+   * @param sizeInMB - The size of the new storage account in MB.
+   * @param owner2 - An optional PublicKey for the second owner of the storage account.
+   * @returns A promise that resolves to a CreateStorageResponse object or null.
+   */
+  const createStorageAccount = useCallback(async (name: string, sizeInMB: number, owner2?: PublicKey) => {
+    return shadowStorage ? await shadowStorage.createStorageAccount(name, `${sizeInMB.toString()}MB`, "v2", owner2) : null;
   }, [shadowStorage]);
 
   const getAvailableStorage = useCallback(async (storageAccount?: PublicKey): Promise<number | null> => {
@@ -123,7 +146,14 @@ export const useShadowStorage = (
     return tx;
   }, [wallet, connection]);
 
-  const uploadData = useCallback(async (data: File | ShadowFile, storageAccount?: PublicKey) => {
+  /**
+   * Upload a file to a storage account, and handle storage and token requirements.
+   *
+   * @param data - The file to be uploaded, which should be of type File or ShadowFile.
+   * @param storageAccount - An optional PublicKey for the target storage account.
+   * @returns A promise that resolves to a ShadowUploadResponse object, null, or undefined.
+   */
+  const uploadData = useCallback(async (data: File | ShadowFile, storageAccount?: PublicKey, makeStorageAccountImmutable: boolean = true) => {
     const requiredSizeInBytes = calculateRequiredSizeInBytes(data);
     if (shadowStorage) {
       if (!storageAccount) {
@@ -147,11 +177,18 @@ export const useShadowStorage = (
           if (shdwBalance < shdwNeeded) {
             await buySHDW(shdwNeeded);
           }
-          const account = await createStorageAccount("Gum", 5);
+          const ownerPublicKey = isSessionWallet(wallet) ? wallet.ownerPublicKey ?? undefined : wallet.publicKey ?? undefined;
+          const account = await createStorageAccount("Gum", 5, ownerPublicKey);
           if (account) {
             storageAccount = new PublicKey(account.shdw_bucket);
           } else {
             throw new Error("Failed to create storage account");
+          }
+          if (makeStorageAccountImmutable) {
+            const response = await shadowStorage.makeStorageImmutable(storageAccount, "v2");
+            if (!response) {
+              throw new Error("Failed to make storage account immutable");
+            }
           }
         }
       }
