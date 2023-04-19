7a4f765d6163c9f31ba4b2f382b2685bc92b1773
Vijay Gupta <vjgupta57@gmail.com>
2023-04-19 11:41:04 +0530

    feat(session): remove create() and revoke() from node sdk

--- packages/gpl-core/src/sessionTokenManager.ts
+++ packages/gpl-core/src/sessionTokenManager.ts
@@ -1,7 +1,6 @@
 import * as anchor from "@project-serum/anchor";
 import { Wallet } from "@project-serum/anchor/dist/cjs/provider";
-import { PublicKey, Keypair, Cluster, Transaction, sendAndConfirmTransaction } from "@solana/web3.js";
-import { BN } from "@project-serum/anchor";
+import { PublicKey, Cluster } from "@solana/web3.js";
 import gpl_session_idl from "./idl/gpl_session.json";
 import { GPLSESSION_PROGRAMS } from "./constants";
 
@@ -25,84 +24,5 @@ export class SessionTokenManager {
 
   public async get(sessionAccount: PublicKey) {
     return this.program.account.sessionToken.fetch(sessionAccount);
-  }
-
-  private async create(sessionSignerKeypair: Keypair, targetProgramPublicKey: PublicKey, ownerPublicKey: PublicKey, topUp: boolean = false, validUntilTimestamp: number | null = null) {
-    const sessionSignerPublicKey = sessionSignerKeypair.publicKey;
-
-    let validUntilBN: BN | null = null;
-    if (validUntilTimestamp !== null) {
-      validUntilBN = new BN(validUntilTimestamp);
-    }
-
-    const instructionMethodBuilder = this.program.methods.createSession(topUp, validUntilBN)
-      .accounts({
-        targetProgram: targetProgramPublicKey,
-        sessionSigner: sessionSignerPublicKey,
-        authority: ownerPublicKey,
-      });
-
-    const pubKeys = await instructionMethodBuilder.pubkeys();
-    const sessionPDA = pubKeys.sessionToken as PublicKey;
-
-    return {
-      instructionMethodBuilder: instructionMethodBuilder,
-      sessionPDA: sessionPDA,
-    };
-  }
-
-  public async createSession(sessionSignerKeypair: Keypair, targetProgramPublicKey: PublicKey, ownerPublicKey: PublicKey, topUp = false, validUntilTimestamp: number | null = null) {
-    try {
-      const { instructionMethodBuilder, sessionPDA } = await this.create(sessionSignerKeypair, targetProgramPublicKey, ownerPublicKey, topUp, validUntilTimestamp);
-
-      return {
-        instructionMethodBuilder: instructionMethodBuilder,
-        sessionPDA: sessionPDA,
-      };
-    } catch (error) {
-      console.error('Error creating session:', error);
-      throw new Error('Failed to create session.');
-    }
-  }
-
-  public async revoke(sessionSignerKeypair: Keypair, sessionAccount: PublicKey, owner: PublicKey) {
-    try {
-      // Transfer all SOL from sessionSigner to owner
-      const sessionWallet = new anchor.Wallet(sessionSignerKeypair);
-      const sessionSignerPublicKey = sessionSignerKeypair.publicKey;
-      const sessionSignerSolanaBalance = await this.provider.connection.getBalance(sessionSignerPublicKey);
-      if (sessionSignerSolanaBalance > 0) {
-        const transferTx = new Transaction().add(anchor.web3.SystemProgram.transfer({
-          fromPubkey: sessionSignerPublicKey,
-          toPubkey: owner,
-          lamports: sessionSignerSolanaBalance,
-        }));
-        transferTx.feePayer = sessionSignerPublicKey;
-        transferTx.recentBlockhash = (await this.provider.connection.getLatestBlockhash()).blockhash;
-        const estimatedFee = await transferTx.getEstimatedFee(this.provider.connection);
-
-        if (sessionSignerSolanaBalance > estimatedFee) {
-          const transaction = new Transaction().add(anchor.web3.SystemProgram.transfer({
-            fromPubkey: sessionSignerPublicKey,
-            toPubkey: owner,
-            lamports: sessionSignerSolanaBalance - estimatedFee,
-          }));
-          transaction.feePayer = sessionSignerPublicKey;
-          transaction.recentBlockhash = (await this.provider.connection.getLatestBlockhash()).blockhash;
-          await sessionWallet.signTransaction(transaction);
-          await sendAndConfirmTransaction(this.provider.connection, transaction, [sessionSignerKeypair]);
-        }
-      }
-
-      // Revoke session
-      return this.program.methods.revokeSession()
-        .accounts({
-          sessionToken: sessionAccount,
-          authority: owner,
-        });
-    } catch (error) {
-      console.error('Error revoking session:', error);
-      throw new Error('Failed to revoke session.');
-    }
   }
 }
