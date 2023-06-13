5eaa179d6de7cec3bda6ac7e58b8ba18d340be90
Vijay Gupta <vjgupta57@gmail.com>
2023-06-13 23:03:10 +0530

    feat: update profile class and graphql queries

--- packages/gpl-core/src/profile.ts
+++ packages/gpl-core/src/profile.ts
@@ -4,8 +4,12 @@ import * as anchor from "@project-serum/anchor";
 import randomBytes from "randombytes";
 
 export interface GumDecodedProfile {
-  user: string;
   address: string;
+  screen_name: string;
+  authority: string;
+  metadata_uri: string;
+  slot_created_at: number;
+  slot_updated_at: number;
 }
 
 export class Profile {
@@ -19,56 +23,45 @@ export class Profile {
     return await this.sdk.program.account.profile.fetch(profileAccount);
   }
 
-  // /**
-  //  * @deprecated This function is slow and may cause performance issues. Consider using getProfilesByUser instead.
-  //  */
-  // public async getProfileAccountsByUser(user: anchor.web3.PublicKey): Promise<anchor.ProgramAccount<any>[]> {
-  //   const users = await this.sdk.user.getUserAccountsByUser(user);
-  //   const userPDAs = users.map((u) => u.publicKey);
-  //   let profiles = [];
-  //   for (const userPDA of userPDAs) {
-  //     const profile = await this.sdk.program.account.profile.all([
-  //       { memcmp: { offset: 8, bytes: userPDA.toBase58() } },
-  //     ]);
-  //     profiles = [...profiles, ...profile];
-  //   }
-  //   return profiles;
-  // }
 
-  // /**
-  //  * Gets or creates a profile for a given user account and namespace.
-  //  * 
-  //  * To use this method, you must first initialize an instance of the SDK and pass a GraphQL client to the constructor.
-  //  * The client will be used to fetch profile information.
-  //  */
-  // public async getOrCreate(
-  //   metadataUri: String,
-  //   screenNameAccount: anchor.web3.PublicKey): Promise<anchor.web3.PublicKey> {
-  //   try {
-  //     const profile = await this.getProfile(userAccount, namespace);
-  //     if (profile?.user && profile?.namespace && profile?.address) {
-  //       const { address: profilePDAstr } = profile;
-  //       return new anchor.web3.PublicKey(profilePDAstr);
-  //     }
-
-  //     const profilePDA = await this.createProfileWithProfileMetadata(metadataUri, userAccount, namespace, owner);
-  //     return profilePDA;
-  //   } catch (e) {
-  //     throw new Error(`Error getting or creating profile: ${e.message}`);
-  //   }
-  // }
+  /**
+   * Gets or creates a profile for a given user account and namespace.
+   * 
+   * To use this method, you must first initialize an instance of the SDK and pass a GraphQL client to the constructor.
+   * The client will be used to fetch profile information.
+   */
+  public async getOrCreate(
+    metadataUri: String,
+    screenNameAccount: anchor.web3.PublicKey,
+    authority: anchor.web3.PublicKey,
+    payer: anchor.web3.PublicKey = authority): Promise<anchor.web3.PublicKey> {
+    try {
+      const profile = await this.getProfile(screenNameAccount, authority);
+      if (profile?.address) {
+        const { address: profilePDAstr } = profile;
+        return new anchor.web3.PublicKey(profilePDAstr);
+      }
+      const { profilePDA, instructionMethodBuilder } = await this.create(metadataUri, screenNameAccount, authority, payer);
+      await instructionMethodBuilder.rpc();
+      return profilePDA;
+    } catch (e) {
+      throw new Error(`Error getting or creating profile: ${e.message}`);
+    }
+  }
 
   public async create(
     metadataUri: String,
     screenNameAccount: anchor.web3.PublicKey,
-    authority: anchor.web3.PublicKey) {
+    authority: anchor.web3.PublicKey,
+    payer: anchor.web3.PublicKey = authority) {
     const randomHash = randomBytes(32);
     const instructionMethodBuilder = this.sdk.program.methods
       // @ts-ignore
       .createProfile(randomHash, metadataUri)
       .accounts({
         screenName: screenNameAccount,
-        authority
+        authority,
+        payer,
       });
     const pubKeys = await instructionMethodBuilder.pubkeys();
     const profilePDA = pubKeys.profile as anchor.web3.PublicKey;
@@ -105,107 +98,115 @@ export class Profile {
       })
   }
 
-  // // GraphQL API methods
+  // GraphQL API methods
 
-  // public async getProfile(userAccount: anchor.web3.PublicKey): Promise<GumDecodedProfile> {
-  //   const query = gql`
-  //     query GetProfile ($userAccount: String, $namespace: jsonb) {
-  //       profile(
-  //         where: {
-  //           user: { _eq: $userAccount },
-  //           namespace: { _eq: $namespace }
-  //         }
-  //       ) {
-  //         address
-  //         namespace
-  //         user
-  //         slot_created_at
-  //         slot_updated_at
-  //       }
-  //     }`;
+  public async getProfile(screenNameAccount: anchor.web3.PublicKey, authority: anchor.web3.PublicKey): Promise<GumDecodedProfile> {
+    const query = gql`
+      query GetProfile ($screenName: String, $authority: String) {
+        profile(
+          where: {
+            screen_name: { _eq: $screenName },
+            authority: { _eq: $authority }
+          }
+        ) {
+          address
+          screen_name
+          authority
+          metadata_uri
+          slot_created_at
+          slot_updated_at
+        }
+      }`;
 
-  //   const variables = {
-  //     userAccount: userAccount.toBase58(),
-  //     namespace: { [namespace.toLowerCase()]: {} },
-  //   };
+    const variables = {
+      screenName: screenNameAccount.toBase58(),
+      authority: authority.toBase58()
+    };
 
-  //   const data = await this.sdk.gqlClient.request<{ profile: GumDecodedProfile[] }>(query, variables);
-  //   return data.profile[0];
-  // }
+    const data = await this.sdk.gqlClient.request<{ profile: GumDecodedProfile[] }>(query, variables);
+    return data.profile[0];
+  }
 
-  // public async getAllProfiles(): Promise<GumDecodedProfile[]> {
-  //   const query = gql`
-  //     query AllProfiles {
-  //       profile {
-  //         user
-  //         namespace
-  //         address
-  //         slot_created_at
-  //         slot_updated_at
-  //       }
-  //     }`;
-  //   const data = await this.sdk.gqlClient.request<{ profile: GumDecodedProfile[] }>(query);
-  //   return data.profile;
-  // }
+  public async getAllProfiles(): Promise<GumDecodedProfile[]> {
+    const query = gql`
+      query AllProfiles {
+        profile {
+          address
+          screen_name
+          authority
+          metadata_uri
+          slot_created_at
+          slot_updated_at
+        }
+      }`;
+    const data = await this.sdk.gqlClient.request<{ profile: GumDecodedProfile[] }>(query);
+    return data.profile;
+  }
 
-  // public async getProfilesByUser(userPubkey: anchor.web3.PublicKey): Promise<GumDecodedProfile[]> {
-  //   const users = await this.sdk.user.getUserAccountsByAuthority(userPubkey);
-  //   const userPDAs = users.map(user => user.address) as string[];
-  //   const query = gql`
-  //     query UserProfiles {
-  //       profile(
-  //         where: {user: {_in: [${userPDAs.map(pda => `"${pda}"`).join(',')}] }}
-  //       ) {
-  //         user
-  //         namespace
-  //         address
-  //         slot_created_at
-  //         slot_updated_at
-  //       }
-  //     }
-  //     `;
-  //   const data = await this.sdk.gqlClient.request<{ profile: GumDecodedProfile[] }>(query);
-  //   return data.profile;
-  // }
+  public async getProfilesByAuthority(authority: anchor.web3.PublicKey): Promise<GumDecodedProfile[]> {
+      const query = gql`
+        query ProfilesByAuthority ($authority: String) {
+          profile(
+            where: {
+              authority: { _eq: $authority }
+            }
+          ) {
+            address
+            screen_name
+            authority
+            metadata_uri
+            slot_created_at
+            slot_updated_at
+          }
+        }
+        `;
+      const variables = {
+        authority: authority.toBase58(),
+      };
 
-  // public async getProfilesByNamespace(namespace: Namespace): Promise<GumDecodedProfile[]> {
-  //   const namespaceString = { [namespace.toLowerCase()]: {} };
-  //   const query = gql`
-  //     query ProfilesByNamespace ($namespace: jsonb) {
-  //       profile(where: { namespace: { _eq: $namespace } }) {
-  //         user
-  //         namespace
-  //         address
-  //         slot_created_at
-  //         slot_updated_at
-  //       }
-  //     }
-  //   `;
-  //   const data = await this.sdk.gqlClient.request<{ profile: GumDecodedProfile[] }>(query, { namespace: namespaceString });
-  //   return data.profile;
-  // }
+      const data = await this.sdk.gqlClient.request<{ profile: GumDecodedProfile[] }>(query, variables);
+      return data.profile;
+  }
 
-  // public async getProfilesByUserAndNamespace(userPubkey: anchor.web3.PublicKey, namespace: Namespace): Promise<GumDecodedProfile> {
-  //   const users = await this.sdk.user.getUserAccountsByAuthority(userPubkey);
-  //   const userPDAs = users.map(user => user.address) as string[];
-  //   const namespaceString = { [namespace.toLowerCase()]: {} };
-  //   const query = gql`
-  //     query ProfileByUserAndNamespace ($namespace: jsonb) {
-  //       profile(
-  //         where: {
-  //           user: {_in: [${userPDAs.map(pda => `"${pda}"`).join(',')}] },
-  //           namespace: { _eq: $namespace }
-  //         }
-  //       ) {
-  //         user
-  //         namespace
-  //         address
-  //         slot_created_at
-  //         slot_updated_at
-  //       }
-  //     }
-  //   `;
-  //   const data = await this.sdk.gqlClient.request<{ profile: GumDecodedProfile[] }>(query, { namespace: namespaceString });
-  //   return data.profile[0];
-  // }
+  public async getProfilesByScreenName(screenNameAccount: anchor.web3.PublicKey): Promise<GumDecodedProfile[]> {
+      const query = gql`
+        query ProfilesByScreenName ($screenName: String) {
+          profile(where: { screen_name: { _eq: $screenName } }) {
+            address
+            screen_name
+            authority
+            metadata_uri
+            slot_created_at
+            slot_updated_at
+          }
+        }
+      `;
+      const variables = {
+        screenName: screenNameAccount.toBase58(),
+      };
+
+      const data = await this.sdk.gqlClient.request<{ profile: GumDecodedProfile[] }>(query, variables);
+      return data.profile;
+  }
+
+  public async getProfilesByProfileAccount(profileAccount: anchor.web3.PublicKey): Promise<GumDecodedProfile[]> {
+      const query = gql`
+        query ProfilesByProfileAccount ($profileAccount: String) {
+          profile(where: { address: { _eq: $profileAccount } }) {
+            address
+            screen_name
+            authority
+            metadata_uri
+            slot_created_at
+            slot_updated_at
+          }
+        }
+      `;
+      const variables = {
+        profileAccount: profileAccount.toBase58(),
+      };
+
+      const data = await this.sdk.gqlClient.request<{ profile: GumDecodedProfile[] }>(query, variables);
+      return data.profile;
+  }
 }
