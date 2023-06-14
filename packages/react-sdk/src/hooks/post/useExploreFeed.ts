f2ff59dca3dd842e685dedfd37d824479e815780
Vijay Gupta <vjgupta57@gmail.com>
2023-06-15 01:26:36 +0530

    feat: update post react hook

--- packages/react-sdk/src/hooks/post/useExploreFeed.ts
+++ packages/react-sdk/src/hooks/post/useExploreFeed.ts
@@ -1,31 +1,30 @@
 import { SDK } from "@gumhq/sdk";
 import { useState, useEffect, useCallback } from "react";
-import { Namespace } from "@gumhq/sdk/lib/profile";
 
-const useExploreFeed = (sdk: SDK, namespace: Namespace) => {
+const useExploreFeed = (sdk: SDK) => {
   const [exploreFeedData, setExploreFeedData] = useState<any>(null);
   const [exploreFeedLoading, setExploreFeedLoading] = useState(false);
   const [exploreFeedError, setExploreFeedError] = useState<Error | null>(null);
 
-  const fetchExploreFeed = useCallback(
-    async () => {
-      setExploreFeedLoading(true);
-      setExploreFeedError(null);
+  // const fetchExploreFeed = useCallback(
+  //   async () => {
+  //     setExploreFeedLoading(true);
+  //     setExploreFeedError(null);
 
-      try {
-        const data = await sdk.post.getPostsByNamespace(namespace);
+  //     try {
+  //       const data = await sdk.post.getPostsByNamespace(namespace);
 
-        setExploreFeedData(data);
-      } catch (err: any) {
-        setExploreFeedError(err);
-      } finally {
-        setExploreFeedLoading(false);
-      }
-    }, [sdk, namespace]);
+  //       setExploreFeedData(data);
+  //     } catch (err: any) {
+  //       setExploreFeedError(err);
+  //     } finally {
+  //       setExploreFeedLoading(false);
+  //     }
+  //   }, [sdk]);
 
-  useEffect(() => {
-    fetchExploreFeed();
-  }, []);
+  // useEffect(() => {
+  //   fetchExploreFeed();
+  // }, []);
 
   return { exploreFeedData, exploreFeedLoading, exploreFeedError };
 };
